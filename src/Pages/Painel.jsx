2ad362ac859aad27a34a2674c754c29f11d98649
Vitrola <vitrola.aotn@gmail.com>
2023-12-05 08:38:29 -0300

    first commit

---
+++ src/Pages/Painel.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header"; 
+import Conteudo from "../components/Conteudo";
+import Tabela from "../components/Tabela";
+function Painel() {
+    return (
+        <>
+        <Header btn={true} />
+        <Conteudo>
+            <h2>Lista de Funcionarios</h2>
+            <Tabela/>
+        </Conteudo>
+        </> 
+    );
+}
+
+export default Painel;
---
+++ src/components/Tabela.jsx
@@ -0,0 +1,23 @@
+import Table from 'react-bootstrap/Table'
+
+function Tabela() {
+    return(
+       
+    <>
+        <Table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Cargo</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+        </Table>
+    </>
+    )
+}
+
+
+export default Tabela;
