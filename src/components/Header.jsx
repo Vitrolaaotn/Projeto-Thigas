2cbd33845ecd69e98c82d899f5725b4448758f15
Vitrola <vitrola.aotn@gmail.com>
2023-12-12 15:53:49 -0300

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Conteiner from 'react-bootstrap/Container' 
 import Button from 'react-bootstrap/Button'
+import Adicionar from './Adicionar';
+import { useState } from 'react';
+
 function Header({btn}) {
+
+    const [show, setShow] = useState(false);
 
     function sair() {
         localStorage.removeItem('key');
@@ -16,11 +21,12 @@ function Header({btn}) {
                     </Navbar.Brand>
                     {btn ? (
                     <div className='d-flex gap-3'>
-                        <Button variant="outline-light">Adicionar</Button>
+                        <Button onClick= {() => setShow(true)} variant="outline-light">Adicionar</Button>
                         <Button onClick={sair} variant="outline-light">Sair</Button>
                     </div>): ''}
                     
                 </Conteiner>
+                <Adicionar show={show} setShow={setShow}/>
             </Navbar>
         </header>
     );
